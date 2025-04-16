7fa6f872dd0b73f994c86863f294e116bb2d825d
u_m26tc <osolovyev@alfabank.ru>
2025-04-16 22:16:08 +0300

    commit

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -8,11 +8,11 @@ export const ThxLayout = () => {
       <div className={thxSt.container}>
         <img src={heart} width={280} height={280} className={thxSt.rocket} />
         <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="medium" weight="semibold">
-          Сервис ещё в работе
+          Alfa Only
         </Typography.TitleResponsive>
         <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-          Мы выбираем лучший подход, чтобы вам точно понравилось. С нетерпением ждём, когда всё заработает, чтобы вам
-          показать.
+          Для подключения свяжитесь
+          с персональным менеджером
         </Typography.Text>
       </div>
     </>
